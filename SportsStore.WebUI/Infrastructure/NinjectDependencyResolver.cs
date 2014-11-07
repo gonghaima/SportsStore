cd3f7d0de5dd3b6038df0bf4d73f07a3309ff6ff
Steven <gonghaima@hotmail.com>
2014-11-08 00:43:18 +1300

    Registering the Implementation 247

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web.Mvc;
 using Moq;
@@ -32,15 +33,13 @@ namespace SportsStore.WebUI.Infrastructure
         }
         private void AddBindings()
         {
-//            Mock<IProductRepository> mock = new Mock<IProductRepository>
-//();
-//            mock.Setup(m => m.Products).Returns(new List<Product> {
-//new Product { Name = "Football", Price = 25 },
-//new Product { Name = "Surf board", Price = 179 },
-//new Product { Name = "Running shoes", Price = 95 }
-//});
-//            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
             kernel.Bind<IProductRepository>().To<EFProductRepostory>();
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager.AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+                .WithConstructorArgument("settings",emailSettings);
         }
     }
 }
